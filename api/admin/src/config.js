66249afb01dbeb348e7189a59946046d3bbf78e4
Rajkumarbitling <rajkumarbitling@gmail.com>
2023-02-08 12:46:21 +0000

    api url change

--- api/admin/src/config.js
+++ api/admin/src/config.js
@@ -2,5 +2,5 @@ import axios from "axios";
 
 export const axiosInstance = axios.create({
     baseURL: "https://react-movie-app-23vhrdtuz-rajkumarbitling.vercel.app/api/",
-    // proxy: "http://localhost:8800/api/",
+    // proxy: "https://react-movie-app-23vhrdtuz-rajkumarbitling.vercel.app/api/",
 })

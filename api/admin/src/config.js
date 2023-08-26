c34564d8b5377090cf64ddde4bccf61cab1c0571
Rajkumarbitling <rajkumarbitling@gmail.com>
2023-08-26 22:15:21 +0530

    vercel changes

--- api/admin/src/config.js
+++ api/admin/src/config.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    // baseURL: "https://rajbits-movie-app.herokuapp.com/api/",
-    proxy: "http://localhost:8800/api/",
+    baseURL: "https://react-movie-app-api.vercel.app/api/",
+    // baseURL: "http://localhost:8800/api/",
+    // proxy: "https://react-movie-app-api.vercel.app/api/",
 })
